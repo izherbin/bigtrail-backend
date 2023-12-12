f7b0299da340c24e84e6c8f14a5c309c816db797
Maxim Piskunov <qualaddar@gmail.com>
2023-12-12 09:16:31 +0200

    Add SMS to early prompt

--- src/app/auth/user.service.ts
+++ src/app/auth/user.service.ts
@@ -1,9 +1,14 @@
-import { ForbiddenException, Injectable } from '@nestjs/common'
+import {
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Injectable
+} from '@nestjs/common'
 import { LoginPhoneInput } from './dto/login-phone.input'
 import { User } from './entities/user.entity'
 import { ConfigService } from '@nestjs/config'
 import { HttpService } from '@nestjs/axios'
-import { catchError /*, lastValueFrom, map */ } from 'rxjs'
+import { catchError /*, lastValueFrom, map */, lastValueFrom, map } from 'rxjs'
 
 @Injectable()
 export class UserService {
@@ -14,35 +19,44 @@ export class UserService {
   ) {}
 
   async createUser(loginPhoneInput: LoginPhoneInput) {
+    const tsCheck = Date.now()
+
+    if (
+      this.user.tsSMSSent &&
+      tsCheck - this.user.tsSMSSent <
+        Number(this.configService.get<string>('NEW_SMS_TIMEOUT'))
+    ) {
+      throw new HttpException('SMS too early', HttpStatus.CONFLICT)
+    }
+
     this.user.phone = loginPhoneInput.phone
 
     this.user.code = Math.round(Math.random() * 899999 + 100000)
-    console.log('!!!!!this.user.code:', this.user.code)
 
-    const request = this.http
-      .post(
-        'https://api.releans.com/v2/message',
-        {},
-        {
-          params: {
-            sender: this.configService.get<string>('SMS_SENDER_NAME'),
-            mobile: '+' + loginPhoneInput.phone,
-            content: 'Your code is : ' + this.user.code
-          },
-          headers: {
-            Authorization:
-              'Bearer ' + this.configService.get<string>('SMS_API_KEY')
-          }
-        }
-      )
-      // .pipe(map((res) => res.status))
-      .pipe(
-        catchError(() => {
-          throw new ForbiddenException('API not available')
-        })
-      )
+    // const request = this.http
+    //   .post(
+    //     'https://api.releans.com/v2/message',
+    //     {},
+    //     {
+    //       params: {
+    //         sender: this.configService.get<string>('SMS_SENDER_NAME'),
+    //         mobile: '+' + loginPhoneInput.phone,
+    //         content: 'Your code is : ' + this.user.code
+    //       },
+    //       headers: {
+    //         Authorization:
+    //           'Bearer ' + this.configService.get<string>('SMS_API_KEY')
+    //       }
+    //     }
+    //   )
+    //   .pipe(map((res) => res.status))
+    //   .pipe(
+    //     catchError(() => {
+    //       throw new ForbiddenException('API not available')
+    //     })
+    //   )
     // const status = await lastValueFrom(request)
-    console.log('request:', request)
+    // console.log('status:', status)
 
     this.user.tsSMSSent = Date.now()
 
